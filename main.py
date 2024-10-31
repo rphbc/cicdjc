ae2958bbbe1cca00c07d3759f20d3ebd587f60c5
Raphael Costa <raphael.costa@afry.com>
2024-10-31 15:31:35 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+def soma(a, b):
+    return a + b
+
+def divisao(a, b):
+    try:  
+        return a / b  
+    except ZeroDivisionError:  
+        raise ZeroDivisionError("Division by zero is not allowed")
+
+class Test_soma:
+    def test_soma(self):
+        assert soma(1, 2) == 3
+
+    def test_divisao(self):
+        assert divisao(1, 2) == 0.5
+    
